782ece3c4fb899e13b729d476e5c130425346d1f
Jindrich K. Smitka <jsmitka@smita.info>
2018-01-12 10:29:35 +0100

    Added quality chceck packages

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule Auth.Mixfile do
   use Mix.Project
 
+  @organization "catchcake"
   def project do
     [
       app: :auth,
       version: "0.1.0",
+      organization: @organization,
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps()
@@ -24,6 +26,9 @@ defmodule Auth.Mixfile do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.18.1", only: :dev},
+      {:excoveralls, "~> 0.7", only: :test},
+      {:credo, "~> 0.3", only: [:dev, :test]},
     ]
   end
 end
