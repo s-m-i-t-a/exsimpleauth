f15f6a6f3b051db257574a3fa2097531a54e1a50
Jindrich K. Smitka <jsmitka@smita.info>
2018-01-24 10:23:51 +0100

    Added verify docs and tests

--- lib/exsimpleauth/token.ex
+++ lib/exsimpleauth/token.ex
@@ -5,37 +5,79 @@ defmodule ExSimpleAuth.Token do
 
   @empty [nil, ""]
 
-  def verify(token, args \\ []) do
-    key = Keyword.get(args, :key, System.get_env("SECRET_KEY"))
+  @doc """
+  Check the supplied token and it's expiration.
 
-    opts =
+  ## Options
+
+  * `:key` - decryption key. If not used,
+    then read `SECRET_KEY` variable from system environment.
+
+  ## Returns
+
+  * `{:ok, data}` - token is valid.
+  * `{:error, "invalid JWT"}` - invalid token
+  * `{:error, [:exp,...]}` - invalid claim (eg. token expired)
+
+  ## Examples
+
+  returns data if token is valid
+
+      iex> key = "y):'QGE8M-b+MEKl@k4e<;*9.BqL=@~B"
+      ...> data = %{"foo": 1234}
+      ...> token = ExSimpleAuth.Token.generate(data, key: key)
+      ...> ExSimpleAuth.Token.verify(token, key: key)
+      {:ok, %{"foo": 1234}}
+
+  returns expiration error when token expire
+
+      iex> key = "y):'QGE8M-b+MEKl@k4e<;*9.BqL=@~B"
+      ...> data = %{"foo": 1234}
+      ...> token = ExSimpleAuth.Token.generate(data, key: key, expiration: 0)
+      ...> ExSimpleAuth.Token.verify(token, key: key)
+      {:error, [:exp]}
+
+  returns error if token is invalid
+
+      iex> key = "y):'QGE8M-b+MEKl@k4e<;*9.BqL=@~B"
+      ...> data = %{"foo": 1234}
+      ...> token = ExSimpleAuth.Token.generate(data, key: key)
+      ...> key2 = "12345678901234567890123456789012"
+      ...> ExSimpleAuth.Token.verify(token, key: key2)
+      {:error, "invalid JWT"}
+  """
+  def verify(token, opts \\ []) do
+    key = Keyword.get(opts, :key, System.get_env("SECRET_KEY"))
+
+    options =
       %{}
       |> put_key(key)
 
     token
-    |> JwtClaims.verify(opts)
+    |> JwtClaims.verify(options)
+    |> result()
   end
 
-  def generate(data, args \\ []) do
-    key = Keyword.get(args, :key, System.get_env("SECRET_KEY"))
-    expiration = Keyword.get(args, :expiration, 24 * 60 * 60)
+  def generate(data, opts \\ []) do
+    key = Keyword.get(opts, :key, System.get_env("SECRET_KEY"))
+    expiration = Keyword.get(opts, :expiration, 24 * 60 * 60)
 
-    opts =
+    options =
       %{}
       |> put_key(key)
 
     claim()
     |> put_data(data)
     |> put_expiration(expires_in(expiration))
-    |> JsonWebToken.sign(opts)
+    |> JsonWebToken.sign(options)
   end
 
-  defp put_key(_opts, key) when key in @empty do
+  defp put_key(_options, key) when key in @empty do
     raise "Key can't be empty!"
   end
 
-  defp put_key(%{} = opts, key) do
-    Map.put(opts, :key, key)
+  defp put_key(%{} = options, key) do
+    Map.put(options, :key, key)
   end
 
   defp claim() do
@@ -54,5 +96,13 @@ defmodule ExSimpleAuth.Token do
     DateTime.utc_now()
     |> DateTime.to_unix()
     |> Kernel.+(exp)
+  end
+
+  defp result({:ok, %{data: data}}) do
+    {:ok, data}
+  end
+
+  defp result(error) do
+    error
   end
 end
--- test/exsimpleauth_test.exs
+++ test/exsimpleauth_test.exs
@@ -2,7 +2,7 @@ defmodule ExSimpleAuthTest do
   use ExUnit.Case
   doctest ExSimpleAuth
 
-  test "greets the world" do
-    assert ExSimpleAuth.hello() == :world
-  end
+  # test "greets the world" do
+  #   assert ExSimpleAuth.hello() == :world
+  # end
 end
