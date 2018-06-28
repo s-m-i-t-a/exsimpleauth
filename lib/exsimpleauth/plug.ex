b75322587ff6443f80953aa316cde6031446d94a
Jindrich K. Smitka <jsmitka@smita.info>
2018-06-28 20:18:59 +0200

    Added plug documentation

--- lib/exsimpleauth/plug.ex
+++ lib/exsimpleauth/plug.ex
@@ -1,6 +1,28 @@
 defmodule ExSimpleAuth.Plug do
   @moduledoc """
   A simple authentication plug.
+
+  ## Options
+
+  * `:http_header` - The name of the HTTP *request* header to check for
+    existing request token. Default value is "x-auth-token".
+  * `:get_user` - The function returns the user.
+    It has input data decoded from the JWT token and returns either `{:ok, user}` or `{:error, message}`.
+  * `:key` - decryption key. If not used,
+    then read `SECRET_KEY` variable from system environment.
+
+  ## Examples
+
+      plug ExSimpleAuth.Plug, http_header: "custom-auth-token", get_user: &MyUser.get/1
+
+
+      iex> key = "y):'QGE8M-b+MEKl@k4e<;*9.BqL=@~B"
+      ...> user = %{id: "1234567890"}
+      ...> token = ExSimpleAuth.Token.generate(user, key: key)
+      ...> defaults = ExSimpleAuth.Plug.init(key: key, get_user: fn %{id: value} -> {:ok, value} end)
+      ...> conn = conn(:get, "/foo") |> put_req_header("x-auth-token", token) |> ExSimpleAuth.Plug.call(defaults)
+      ...> conn.assigns[:user]
+      "1234567890"
   """
 
   alias ExSimpleAuth.Token
@@ -12,16 +34,17 @@ defmodule ExSimpleAuth.Plug do
   def init(opts) do
     token_header = Keyword.get(opts, :http_header, "x-auth-token")
     get_user = Keyword.get(opts, :get_user) || raise "'get_user' must be set"
+    key = Keyword.get(opts, :key, System.get_env("SECRET_KEY"))
 
-    {get_user, token_header}
+    {get_user, token_header, key}
   end
 
   @impl true
-  def call(conn, {get_user, token_header}) do
+  def call(conn, {get_user, token_header, key}) do
     conn
     |> Conn.get_req_header(token_header)
     |> token_to_result()
-    |> Result.and_then(&Token.verify/1)
+    |> Result.and_then(fn data -> Token.verify(data, key: key) end)
     |> Result.and_then(get_user)
     |> store_user_or_deny_access(conn)
   end
